ce66f0abdc35f6c3e8c921e889f6d01709dcdfac
Liang Lin <lianglin@outlook.com>
2016-04-18 22:03:40 +0100

    add bubble sorting

---
+++ sorting/bubblesorting.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+ # bubble sort has a "quadratic" worst-case running-time
+ # Θ(n^2)
+def bubbleSort(A):
+    n = len(A)
+    for i in xrange(n-1):
+        for j in xrange(n-1):
+            if A[j] > A[j+1]:
+                tmp    = A[j]
+                A[j]   = A[j+1]
+                A[j+1] = tmp
+    print(A)
+
+bubbleSort([2,1,3,6,9,5])
