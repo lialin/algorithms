1e9d9123a3925c7f73075136f17177b598a554ff
Liang Lin <lianglin@outlook.com>
2016-04-26 18:28:48 +0100

    add py

--- sorting/insertionSort/insertionSort.py
+++ sorting/insertionSort/insertionSort.py
@@ -2,9 +2,19 @@
 # -*- coding: utf-8 -*-
 
 # Author: Liang LIn
-# Email: 
+# Email:
 # Date: 2016-04-26
 
-# Θ(n)
+# Θ(n^2)
 def insertionSort(A):
-    pass
+    n = len(A)
+    for i in range(n-2, -1, -1):
+        key = A[i]
+        j = i + 1
+        while (j < n and key > A[j]):
+            A[j-1] = A[j]
+            j+=1
+        A[j-1] = key
+    print(A)
+
+insertionSort([7,2,9,4,5,10,25,12])
