0103bba1113b16314355e013acf50f963480f97a
Liang Lin <lianglin@outlook.com>
2016-05-03 20:00:59 +0100

    add sorting/mergeSort files

---
+++ sorting/mergeSort/mergeSort.lhs
@@ -0,0 +1,8 @@
+> module MergeSort where
+
+Author: Liang Lin
+Email: lianglin@outlook.com
+Date: 2016-05-03
+
+> mergeSort ::
+> mergeSort
---
+++ sorting/mergeSort/mergeSort.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Author: Liang Lin
+# Email: lianglin@outlook.com
+# Date: 2016-05-03
+
+# Θ(n)
+def mergeSort(A):
+    pass
